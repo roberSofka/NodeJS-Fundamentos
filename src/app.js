5f713f1355870c2f793f620040022a5e3e16508b
Rober Martinez <rober.martinez@sofka.com.co>
2024-08-08 20:51:42 -0500

    se crea archivo gitignore y el env

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
+require("dotenv").config();
+const operaciones = require("./operaciones");
+
 console.log("¡Hola, mundo desde Node.js!");
 
-const operaciones = require("./operaciones");
+const apiKey = process.env.API_KEY;
+const databaseUrl = process.env.DATABASE_URL;
+
 console.log(operaciones.suma(5, 3));
 console.log(operaciones.resta(10, 4));
+console.log(apiKey);
+console.log(databaseUrl);
